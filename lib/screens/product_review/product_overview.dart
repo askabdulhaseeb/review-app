e8b35409263a32b42984fceebe66326923f76301
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-05-18 21:45:41 +0500

    added elevation to review card view

--- lib/screens/product_review/product_overview.dart
+++ lib/screens/product_review/product_overview.dart
@@ -12,70 +12,72 @@ class ProductOverview extends StatelessWidget {
       fontWeight: FontWeight.bold,
       fontSize: 20,
     );
-    return Container(
-      padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.all(10),
-      // color: Colors.yellow,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: size.width * 0.3,
-            width: size.width * 0.3,
-            margin: const EdgeInsets.only(right: 10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                image: NetworkImage(product.imageURL),
-                fit: BoxFit.cover,
+    return Card(
+      elevation: 2,
+      margin: const EdgeInsets.only(bottom: 10),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: size.width * 0.3,
+              width: size.width * 0.3,
+              margin: const EdgeInsets.only(right: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                  image: NetworkImage(product.imageURL),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Container(
-            width: size.width * 0.45,
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  product.title,
-                  overflow: TextOverflow.ellipsis,
-                  style: boldTextStyle,
-                ),
-                Text(
-                  'Rating: ${product.rating.toString()} (${product.votes})',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: (product.rating > 4)
-                        ? Colors.green
-                        : (product.rating > 3)
-                            ? Colors.blue
-                            : (product.rating > 2)
-                                ? Colors.orange
-                                : Colors.red,
+            Container(
+              width: size.width * 0.45,
+              // padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.title,
+                    overflow: TextOverflow.ellipsis,
+                    style: boldTextStyle,
                   ),
-                ),
-                const SizedBox(height: 6),
-                Text(
-                  'Price: ${product.price}',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+                  Text(
+                    'Rating: ${product.rating.toString()} (${product.votes})',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: (product.rating > 4)
+                          ? Colors.green
+                          : (product.rating > 3)
+                              ? Colors.blue
+                              : (product.rating > 2)
+                                  ? Colors.orange
+                                  : Colors.red,
+                    ),
                   ),
-                ),
-                Text(
-                  'Category: $category',
-                  style: TextStyle(
-                    fontSize: 14,
+                  const SizedBox(height: 6),
+                  Text(
+                    'Price: ${product.price}',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          )
-        ],
+                  Text(
+                    'Category: $category',
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
